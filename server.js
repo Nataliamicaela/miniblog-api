b57e50c29971bde3a1636aa4aa1053b00a437553
Natalia Alvarez <nataliamicaela26@hotmail.com>
2026-05-12 13:29:24 -0300

    test: verificar deploy Railway

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
 
   console.log(
-    `Server running on http://localhost:${PORT}`
-  );
+    `RAILWAY TEST OK - PORT ${PORT}`
+ );
 
 });
